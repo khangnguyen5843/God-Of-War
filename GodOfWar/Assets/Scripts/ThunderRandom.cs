2ea24b43318899eafdc4f885fd09f549401bdbe8
HoangTuanHielo <TTuan610@gmail.com>
2023-08-19 18:05:52 +0700

    Làm sấm sét random
    Bài ở sence số 2 nha

---
+++ GodOfWar/Assets/Scripts/ThunderRandom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderRandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject Thunder;
+    void Start()
+    {
+        StartCoroutine(SpawnThunder());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnThunder()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(1);
+            float randomX = Random.value;
+            float randomY = Random.value;
+            Vector3 spawnLocation = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, 0));
+            Instantiate(Thunder, spawnLocation,Quaternion.identity);
+        }
+    }
+}
